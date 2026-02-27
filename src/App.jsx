187be8b512b96fb54fa755fc953520706586fc4b
Ken Udoudo <kendo302003@gmail.com>
2026-02-27 16:24:55 +0100

    Organizing code

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ function App() {
 
   return (
     <Router> 
-    <InventoryProvider>
+    <InventoryProvider className="w-full flex flex-row">
       <Sidebar />
       {/*<div className="content">
           <Routes>
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,11 +1,30 @@
+import { useState } from "react";
+import logo from "../assets/Logo.png";
+
 const Sidebar = () => {
-    return ( 
-        <div>
-            <div>
-                
-            </div>
+  const [active, setActive] = useState("dashboard");
+
+  return (
+    <div className="w-[20%] flex flex-col items-center">
+      <img src={logo} alt="Logo" className="pt-[24px]" />
+
+      <div className="flex flex-col items-center px-[20px] w-full mt-[40px] gap-3">
+
+        <div
+          onClick={() => setActive("dashboard")}
+          className={`w-full text-[14px] text-center py-[15px] rounded-[10px] cursor-pointer transition-all duration-200
+            ${
+              active === "dashboard"
+                ? "bg-[#4880FF] text-white"
+                : "bg-white text-black hover:bg-[#4880FF] hover:text-white"
+            }`}
+        >
+          DashBoard
         </div>
-     );
-}
- 
+
+      </div>
+    </div>
+  );
+};
+
 export default Sidebar;
